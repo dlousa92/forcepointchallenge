895978389066558b9da0031fcab741e4b71ae4aa
Daniel Lousa <dlousa92@gmail.com>
2018-11-07 01:35:01 -0500

    set background color individually based on persons priority attribute

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react'
 import './Item.css'
 
-// A Item will be populated by one person
+let className
 
 class Item extends Component {
   render () {
     // This will contain all of the info for an individual person
     const person = this.props.person
-
     return (
-      <div className='personitem'>
+      <div className={className}>
         <p>{person.name}</p>
         <p>{person.age}</p>
         <p>{person.category}</p>
       </div>
     )
+  }
+
+  componentWillMount () {
+    const person = this.props.person
+
+    // This functions purpose is to set the background color of the individual person dynamically by
+    // checking the persons priority attribute and setting that color conditionally based on it's value.
+    function setClassName () {
+      if (person.priority === 1) {
+        className = 'personitem orange'
+        console.log(className)
+      } else if (person.priority === 2) {
+        className = 'personitem green'
+      } else if (person.priority === 3) {
+        className = 'personitem blue'
+      } else if (person.priority === 4) {
+        className = 'personitem purple'
+      }
+    }
+    setClassName()
   }
 }
 
