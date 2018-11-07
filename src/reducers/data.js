97f5446920044f0c9f4905cc626960a9667309e5
Daniel Lousa <dlousa92@gmail.com>
2018-11-07 16:23:20 -0500

    sort the data by its priority value

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -2,24 +2,34 @@
 
 function data (state = [], action) {
   console.log(state, action)
-
   switch (action.type) {
     case 'SORT_BY_DEFAULT':
-    // return updated state
-      console.log('I am sorting the data by default order')
-      break
-    case 'SORT_ALPHABETICALLY':
-    // return updated state
-      console.log('I am sorting the data alphabetically')
-      break
-    case 'SORT_BY_PRIORITY':
       // return updated state
-      console.log('I am sorting the data by priority order')
-      break
+      console.log('I am sorting the data by default order')
+      return [
+
+      ]
+    case 'SORT_ALPHABETICALLY':
+      // return updated state
+      console.log(state)
+      return [
+
+      ]
+    case 'SORT_BY_PRIORITY':
+      return [
+        // Make a copy of state and compare the priority attribute value of each item in the array. Sort in ascending order.
+        ...state.sort(function (a, b) {
+          if (a.priority < b.priority) {
+            return -1
+          } if (a.priority > b.priority) {
+            return 1
+          }
+          return 0
+        })
+      ]
     default:
       return state
   }
-  return state
 }
 
 export default data
