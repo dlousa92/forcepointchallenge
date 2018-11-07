1ba8ee307fc764845312c45fec089717b833a662
Daniel Lousa <dlousa92@gmail.com>
2018-11-06 20:34:43 -0500

    create action creators for sorting data

---
+++ src/actions/actionCreators.js
@@ -0,0 +1,20 @@
+// sort by default
+export function sortDefault () {
+  return {
+    type: 'SORT_BY_DEFAULT'
+  }
+}
+
+// alphabetically sort
+export function sortAlphabetical () {
+  return {
+    type: 'SORT_ALPHABETICALLY'
+  }
+}
+
+// priority sort
+export function sortPriority () {
+  return {
+    type: 'SORT_BY_PRIORITY'
+  }
+}
