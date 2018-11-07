f354df4052e367c91d9e1eb10cbc1cf0aa7c890e
Daniel Lousa <dlousa92@gmail.com>
2018-11-07 01:46:12 -0500

    pass props to header

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 import './Main.css'
 
 import Grid from '../Grid/Grid'
+import Header from '../Header/Header'
 
 class Main extends Component {
   render () {
     return (
       <div className='main'>
+        <Header {...this.props} />
         <Grid {...this.props} />
       </div>
     )
