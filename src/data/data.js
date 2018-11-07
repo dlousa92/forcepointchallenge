38f16cb2fd7712d99fd4a4acebd4c863c4a1d019
Daniel Lousa <dlousa92@gmail.com>
2018-11-06 19:08:42 -0500

    create data file

---
+++ src/data/data.js
@@ -0,0 +1,114 @@
+const data = {
+  "data": [
+
+    {
+
+      "name":  "Jerry",
+
+      "age": 39,
+
+      "priority": 1,
+
+      "category": "two"
+
+    },
+
+    {
+
+      "name":  "Amy",
+
+      "age": 28,
+
+      "priority": 4,
+
+      "category": "one"
+
+    },
+
+    {
+
+      "name":  "Bernice",
+
+      "age": 99,
+
+      "priority": 2,
+
+      "category": "two"
+
+    },
+
+    {
+
+      "name":  "Antoine",
+
+      "age": 50,
+
+      "priority": 1,
+
+      "category": "three"
+
+    },
+
+    {
+
+      "name":  "Andrea",
+
+      "age": 25,
+
+      "priority": 1,
+
+      "category": "three"
+
+    },
+
+    {
+
+      "name":  "Fredrick",
+
+      "age": 44,
+
+      "priority": 3,
+
+      "category": "one"
+
+    },
+
+    {
+
+      "name":  "Mariel",
+
+      "age": 34,
+
+      "priority": 2,
+
+      "category": "three"
+
+    },
+
+    {
+
+      "name":  "Arnold",
+
+      "age": 74,
+
+      "priority": 4,
+
+      "category": "two"
+
+    },
+
+    {
+
+      "name":  "Janice",
+
+      "age": 54,
+
+      "priority": 2,
+
+      "category": "two"
+
+    }
+
+  ]
+
+}
