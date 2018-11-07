60dd9152245e0f011a5352ca3fa3b14b4de9d9b3
Daniel Lousa <dlousa92@gmail.com>
2018-11-06 20:32:42 -0500

    create grid component

---
+++ src/components/Grid/Grid.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Grid extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <p>
+          Hello, I am the grid
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Grid
