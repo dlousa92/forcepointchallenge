0858bc74997d5e384fc8cfe682c875df03fe0647
Daniel Lousa <dlousa92@gmail.com>
2018-11-06 20:32:50 -0500

    create single component

---
+++ src/components/Single/Single.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Single extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <p>
+          Hello, I am a single person
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Single
