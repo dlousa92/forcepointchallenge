77ccdbb857d3c71520caa84de385d8cfe9cfa537
Daniel Lousa <dlousa92@gmail.com>
2018-11-07 17:32:01 -0500

    sort alphabetically and come up with temp solution for sorting by default

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -1,19 +1,137 @@
-// a reducer takes in two things, it takes in the action and it also takes in a copy of current state
+// This is the default state of the data set. It's copied here so it's possible to reset state to the default way data is ordered
+const DEFAULT_STATE =
+  [
+    {
 
+      'name': 'Jerry',
+
+      'age': 39,
+
+      'priority': 1,
+
+      'category': 'two'
+
+    },
+
+    {
+
+      'name': 'Amy',
+
+      'age': 28,
+
+      'priority': 4,
+
+      'category': 'one'
+
+    },
+
+    {
+
+      'name': 'Bernice',
+
+      'age': 99,
+
+      'priority': 2,
+
+      'category': 'two'
+
+    },
+
+    {
+
+      'name': 'Antoine',
+
+      'age': 50,
+
+      'priority': 1,
+
+      'category': 'three'
+
+    },
+
+    {
+
+      'name': 'Andrea',
+
+      'age': 25,
+
+      'priority': 1,
+
+      'category': 'three'
+
+    },
+
+    {
+
+      'name': 'Fredrick',
+
+      'age': 44,
+
+      'priority': 3,
+
+      'category': 'one'
+
+    },
+
+    {
+
+      'name': 'Mariel',
+
+      'age': 34,
+
+      'priority': 2,
+
+      'category': 'three'
+
+    },
+
+    {
+
+      'name': 'Arnold',
+
+      'age': 74,
+
+      'priority': 4,
+
+      'category': 'two'
+
+    },
+
+    {
+
+      'name': 'Janice',
+
+      'age': 54,
+
+      'priority': 2,
+
+      'category': 'two'
+
+    }
+  ]
+
+// Handles all actions
 function data (state = [], action) {
   console.log(state, action)
   switch (action.type) {
     case 'SORT_BY_DEFAULT':
-      // return updated state
-      console.log('I am sorting the data by default order')
-      return [
 
+      // Default option resets store to the initial state which is declared above. Okay solution for now but shouldn't be final solution
+      return [
+        ...DEFAULT_STATE
       ]
     case 'SORT_ALPHABETICALLY':
-      // return updated state
       console.log(state)
       return [
-
+        // Make a copy of state and compare the name attribute value of each item in the array. Sort by A-Z.
+        ...state.sort(function (a, b) {
+          if (a.name < b.name) {
+            return -1
+          } if (a.name > b.name) {
+            return 1
+          }
+          return 0
+        })
       ]
     case 'SORT_BY_PRIORITY':
       return [
