91c2bf77aa27d60f24eb0fcec3f898a6c8fc55a2
Daniel Lousa <dlousa92@gmail.com>
2018-11-06 19:23:31 -0500

    install initial dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import './index.css'
-import App from './components/App/App'
 import * as serviceWorker from './serviceWorker'
+
+import App from './components/App/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
