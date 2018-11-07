ac3cdb424a2da628e43601b4ca3dc7e3e4296a47
Daniel Lousa <dlousa92@gmail.com>
2018-11-06 23:07:48 -0500

    for each person in the database render a single person square grid

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -2,17 +2,15 @@ import React, { Component } from 'react'
 
 import Single from '../Single/Single'
 
-// The Grid will display all of the Single items inside of it
+// The Grid will display all of the Single person items inside of it
 
 class Grid extends Component {
   render () {
     return (
       <div className='App'>
         <p>
-          Hello, I am the grid
+          {this.props.data.persons.map((person, i) => <Single person={person} key={i} i={i} />)}
         </p>
-
-        <Single />
       </div>
     )
   }
