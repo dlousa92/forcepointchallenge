e900d97792e0514657a09fd0e2b5534e79fc6a64
Daniel Lousa <dlousa92@gmail.com>
2018-11-07 01:46:29 -0500

    create default layout of header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import './Header.css'
+
+import Grid from '../Grid/Grid'
+
+class Header extends Component {
+  render () {
+    return (
+      <div className='header'>
+        <nav>
+          <ul>Sort by:
+            <li><a href='#'>Default</a></li>
+            <li><a href='#'>Alphabetical</a></li>
+            <li><a href='#'>Priority</a></li>
+          </ul>
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default Header
