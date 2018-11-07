4c6cf23b9f86fc1687c17fcc4fd6075b95bfd6d4
Daniel Lousa <dlousa92@gmail.com>
2018-11-06 21:13:15 -0500

    rename appJS to mainJS for semantics

---
+++ src/components/Main/Main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './Main.css'
+
+import Grid from '../Grid/Grid'
+
+class Main extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <Grid />
+      </div>
+    )
+  }
+}
+
+export default Main
