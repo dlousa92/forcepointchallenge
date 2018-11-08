4fb6b1fc9f0f5fa8cdce804a6d99daec0caf4259
Daniel Lousa <dlousa92@gmail.com>
2018-11-08 12:01:25 -0500

    delete unneccsary function and set class name with props instead

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -8,31 +8,12 @@ class Item extends Component {
     // This will contain all of the info for an individual person
     const person = this.props.person
     return (
-      <div className={className}>
+      <div className={`${person.priority} personitem`}>
         <p>{person.name}</p>
         <p>{person.age}</p>
         <p>{person.category}</p>
       </div>
     )
-  }
-
-  componentWillMount () {
-    const person = this.props.person
-
-    // This functions purpose is to set the background color of the individual person dynamically by
-    // checking the persons priority attribute and setting that color conditionally based on it's value.
-    function setClassName () {
-      if (person.priority === 1) {
-        className = 'personitem orange'
-      } else if (person.priority === 2) {
-        className = 'personitem green'
-      } else if (person.priority === 3) {
-        className = 'personitem blue'
-      } else if (person.priority === 4) {
-        className = 'personitem purple'
-      }
-    }
-    setClassName()
   }
 }
 
